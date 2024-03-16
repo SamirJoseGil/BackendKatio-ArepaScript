6dc3ec095321be06ee586c9a575f420d3b6a7791
AlzateJuan <felipealzate76@gmail.com>
2024-03-16 16:33:28 -0500

    Creado entidad y controlador para libros

--- src/main/java/edu/eafit/katio/controllers/BooksController.java
+++ src/main/java/edu/eafit/katio/controllers/BooksController.java
@@ -1,2 +1,18 @@
-package edu.eafit.katio.controllers;public class BooksController {
+package edu.eafit.katio.controllers;
+
+import edu.eafit.katio.models.Book;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/katio/books")
+public class BooksController {
+
+    @GetMapping("/getall")
+    public ResponseEntity<Book> getAll(){
+        return new ResponseEntity<Book>(new Book(), HttpStatus.OK);
+    }
 }
--- src/main/java/edu/eafit/katio/models/Book.java
+++ src/main/java/edu/eafit/katio/models/Book.java
@@ -1,2 +1,69 @@
-package edu.eafit.katio.models;public class Book {
+package edu.eafit.katio.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    private String nombre;
+    private Integer autorId;
+    private String ISBN;
+    private Date fechaPublicacion;
+    private String edicion;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getAutorId() {
+        return autorId;
+    }
+
+    public void setAutorId(Integer autorId) {
+        this.autorId = autorId;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public Date getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(Date fechaDePublicacion) {
+        this.fechaPublicacion = fechaDePublicacion;
+    }
+
+    public String getEdicion() {
+        return edicion;
+    }
+
+    public void setEdicion(String edicion) {
+        this.edicion = edicion;
+    }
 }
